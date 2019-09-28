2d0f470ed3e76539260a0394d352e7c43ccf1563
MatheusHolanda07 <matheus.lopes8807@gmail.com>
2019-09-28 14:59:36 -0300

    6 e 7 adc

---
+++ questoes/q6/Main.java
@@ -0,0 +1,9 @@
+package q6;
+
+public class Main {
+	public static void main(String[] args) {
+		Estoque estoque = new Estoque();
+		
+		
+	}
+}
---
+++ questoes/q7/Estoque.java
@@ -0,0 +1,45 @@
+package q7;
+
+public class Estoque {
+	String nome;
+	int qtdAtual;
+	int qtdMinima;
+	
+	public Estoque(){
+		
+	}
+	
+	public Estoque(String nome, int qtdAtual, int qtdMinima){
+		
+	}
+	
+	 void mudarNome(String nome){
+		
+	}
+	 
+	 void mudarQtdMinima(int qtdMinima){
+		
+	}
+	 
+	 void repor(int qtd){
+		 qtdAtual += qtd;
+	}
+	 
+	 void darBaixa(int qtd){
+		 qtdAtual -=qtd;
+	}
+	 
+	 String mostra(String nome, int qtdAtual, int qtdMinima){
+		System.out.println("produto -> "+nome+"\nquantidade atual-> "+qtdAtual+"\nquantidade minima-> "+qtdMinima);
+		return nome;
+	}
+	 
+	 boolean precisaRepor(){
+		 if(qtdAtual <=qtdMinima){
+			 return true;
+		 }else{
+			 return false;
+		 }
+	 }
+	
+}
